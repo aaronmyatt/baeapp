67f392b2bc78e60d5e6cf5290b247a178e956a62
Aaron Myatt <aaronmyatt@gmail.com>
2016-10-21 22:43:02 +0800

    tidy up location spec

---
+++ spec/location.service-spec.js
@@ -0,0 +1,27 @@
+describe("locationsApi", () => {
+
+    beforeEach(module("location.service"))
+    beforeEach(() => {
+      module(($provide) => {})
+      inject(($injector) => {
+        service = $injector.get("locationApi")
+        httpBackend = $injector.get("$httpBackend")
+      });
+    });
+
+    const response = {"address":"Wisma Academy\n4A, Jalan 19/1","city":"Petaling Jaya","country":"Malaysia","postalCode":"46300", "lng":101.6278914, "lat":3.1120654}
+
+    it("request 1 item from json server", () => {
+        httpBackend.expectGET("localhost:3000/locations/1")
+            .respond(() => { return [] })
+        service.get(1)
+        httpBackend.flush()
+    })
+
+    it("request 1 item from json server", () => {
+        httpBackend.expectGET("localhost:3000/locations")
+            .respond(() => { return [] })
+        service.getAll()
+        httpBackend.flush()
+    })
+})
